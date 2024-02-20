32bb3b1f7fc80b537115dcfbd4bc5b0ea6f5e6ae
Natalie Bo <nataliabo@hotmail.co.uk>
2024-02-20 12:19:52 +0000

    feat: created the array of questions for the user with the extensive licence list options from GitHub drop down list.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    { type: 'input', name: 'title', message: 'What is the title of your project?' },
+    { type: 'input', name: 'description', message: 'Provide a brief overview of your project:' },
+    { type: 'input', name: 'installation', message: 'How is your project installed?' },
+    { type: 'input', name: 'usage', message: 'What is the purpose of your project?' },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select the license for your project:',
+        choices: ['Apache License 2.0',
+        'GNU General Public License v3.0',
+        'MIT License',
+        'BSD 2-Clause "Simplified" License',
+        'BSD 3-Clause "New" or "Revised" License',
+        'Boost Software License 1.0',
+        'Creative Commons Zero v1.0 Universal',
+        'Eclipse Public License 2.0',
+        'GNU Affero General Public License v3.0',
+        'GNU General Public License v2.0',
+        'GNU Lesser General Public License v2.1',
+        'Mozilla Public License 2.0',
+        'The Unlicense',
+        'None']
+    },
+    { type: 'input', name: 'contributing', message: 'How may others contribute to your project?' },
+    { type: 'input', name: 'tests', message: 'How can your project be tested?' },
+    { type: 'input', name: 'github', message: 'Fill in your GitHub username:' },
+    { type: 'input', name: 'email', message: 'Enter your email address:' }
 ];
 
 // function to write README file
